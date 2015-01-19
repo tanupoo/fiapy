9872047b02045750fb2586293a9cbbff38ca8629
Shiochi Sakane <sakane@tanu.org>
2015-01-19 17:05:23 +0900

    updated

--- sample/sample-interface-stat.py
+++ sample/sample-interface-stat.py
@@ -5,9 +5,9 @@ import subprocess
 import shlex
 import re
 import json
-
+import dateutil.parser, dateutil.tz
 from datetime import datetime
-import pytz
+
 TZ='Asia/Tokyo'
 
 args1 = shlex.split('/usr/sbin/netstat -inb')
@@ -16,21 +16,26 @@ args2 = shlex.split('grep -v Link')
 p1 = subprocess.Popen(args1, stdout=subprocess.PIPE)
 p2 = subprocess.Popen(args2, stdin=p1.stdout, stdout=subprocess.PIPE)
 
-pid_base = 'http://tanu.org/mac/if'
+pid_base = 'http://fiap.tanu.org/mac/if'
 
-req = { 'fiap' : '20140401', 'dataRQ' : [] }
-time = datetime.now(pytz.timezone(TZ)).strftime('%Y-%m-%dT%H:%M:%S%z')
+req = { 'fiap': { 'version': '20140401', 'dataRQ' : {} } }
+time = datetime.now()
+time = time.replace(tzinfo=dateutil.tz.gettz(TZ)).strftime('%Y-%m-%dT%H:%M:%S%z')
 
 for line in p2.stdout:
   p = re.split('\s+', line)
   if '.' in p[3]:
+    dataRQ = {}
+    # ibytes
     fiap_name = '%s/%s/ibytes' % (pid_base, p[0])
     value = str(p[6])
-    req['dataRQ'].append({ fiap_name : [ {'time': time, 'value': str(value) } ] })
+    dataRQ[fiap_name] = [ {'time': time, 'value': str(value) } ]
 
     fiap_name = '%s/%s/obytes' % (pid_base, p[0])
     value = str(p[9])
-    req['dataRQ'].append({ fiap_name : [ {'time': time, 'value': str(value) } ] })
+    dataRQ[fiap_name] = [ {'time': time, 'value': str(value) } ]
+
+    req['fiap']['dataRQ'] = dataRQ
 
 print json.dumps(req)
 
