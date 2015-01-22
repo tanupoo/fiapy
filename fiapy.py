5e967a7389a78380b007a3c201b471e87627f52f
Shiochi Sakane <sakane@tanu.org>
2015-01-23 00:11:54 +0900

    improved content-length check. improved adding header fields. improved debug messages.

--- fiapy.py
+++ fiapy.py
@@ -9,22 +9,78 @@ import ssl
 import fiapProto
 import fiapConfig
 
+FIAPY_TIMEOUT = 10
+
 cf = None
 
+# cf.debug
+#   0: normal
+#   1: verbose (mainly, ieee1888 level)
+#   2: more verbose
+
 class fiapHandler(BaseHTTPRequestHandler):
+
+    check_content_length = False
+    check_content_type = False
 
     def __init__(self, request, client_address, server):
         BaseHTTPRequestHandler.__init__(self, request, client_address, server)
 
-    def _log_initmsg(self):
-        if self.headers.has_key('Content-Type') == True:
-            self.ctype = self.headers['Content-Type']
+    #
+    # check content-length, content-type.
+    # set the read timeout.
+    #
+    def _check_request(self):
+        self.log_message('Connection from %s[%d] ' % self.client_address)
+        if cf.debug >= 2:
+            self.log_message('DEBUG: === Headers BEGIN:')
+            for i in self.headers.items():
+                self.log_message(i.__repr__())
+            self.log_message('DEBUG: === Headers END')
+        #
+        # check the content-length
+        #
+        self.clen = -1
+        if self.headers.getheader('Transfer-Encoding') == 'chunked':
+            # XXX here possible DOS happen.
+            self.clen = self.rfile.readline()
+            self.clen = int(self.clen, 16)
         else:
+            self.clen = self.headers.getheader('Content-Length', -1)
+            self.clen = int(self.clen)
+            if self.clen == -1 and self.check_content_length == True:
+                self.log_message('ERROR: Content-Length does not exist')
+                self.send_error(411)
+                return False
+        #
+        # check the content-type
+        #
+        self.ctype = self.headers.getheader('Content-Type')
+        if not self.ctype:
+            if self.check_content_type == True:
+                self.log_message('ERROR: Content-Type does not exist')
+                self.send_error(400)
+                return False
             self.ctype = 'text/html' # default content-type to be parsed.
-        m = 'client=%s[%d] ' % self.client_address
-        m += 'path=[%s] ' % self.path
-        m += 'ctype=[%s]' % self.ctype
-        self.log_message(m)
+            self.log_message('WARNING: Content-Type does not exist, set %s' % self.ctype)
+        #
+        self.log_message('%s %s [%s]' % (self.protocol_version, self.command, self.path))
+        self.rfile._sock.settimeout(FIAPY_TIMEOUT)
+        return True
+
+    #
+    # send 200 OK response
+    #
+    def _send_response(self, doc, ctype):
+        #self.protocol_version = 'HTTP/1.1'
+        doc += '\r\n'
+        self.send_response(200)
+        self.send_header('Content-Length', len(doc))
+        self.send_header('Content-Type', ctype)
+        self.end_headers()
+        self.wfile.write(doc)
+        if cf.debug >= 1:
+            self.log_message('DEBUG: reply body=%s' % doc)
 
     def _check_acl_san(self):
         #
@@ -36,35 +92,24 @@ class fiapHandler(BaseHTTPRequestHandler):
         return cf.check_acl_san(self.request.getpeercert())
 
     def do_POST(self):
-        self._log_initmsg()
+        if self._check_request() == False:
+            return
         if opt.sec_lv and self._check_acl_san() == False:
             self.send_error(401)
             return
         fiap = fiapProto.fiapProto(requester_address=self.client_address, strict_check=True, debug=cf.debug)
-        #
-        # get content-length of the request
-        #
-        clen = 0
-        if self.headers.getheader('Transfer-Encoding', None) == 'chunked':
-            clen = self.rfile.readline()
-            clen = int(clen, 16)
+        if self.clen == -1 and False:
+            #
+            # XXX GUTP interop hack: accept in case of no content-length.
+            #
+            s = self.rfile.read()
         else:
-            clen = self.headers.getheader('Content-Length', None)
-        if clen == None:
-            if False:
-                self.send_error(411)
-                return
-            else:
-                # XXX hack: no content-length
-                s = self.rfile.read()
-        else:
-            clen = int(clen)
-            s = self.rfile.read(clen)
-        # XXX should implement timeout() if clen is more than the actual length.
+            s = self.rfile.read(self.clen)
         #post_data = urlparse.parse_qs(s.rfile.read(length).decode('utf-8'))
         doc = None
         if cf.debug > 0:
-            self.log_message('DEBUG: post body (len=%d): %s' % (clen, s.replace('\n\r','')))
+            self.log_message('DEBUG: post body (len=%d): %s' %
+                    (self.clen, s.replace('\n\r','')))
         if self.ctype.find('text/xml') != -1: # XXX should it compare with 0 ?
             doc = fiap.serverParseXML(s)
         elif self.ctype.find('text/json') != -1:
@@ -78,32 +123,19 @@ class fiapHandler(BaseHTTPRequestHandler):
             self.log_message('ERROR: %s' % fiap.getemsg())
             self.send_error(403, fiap.getemsg())
             return
-        self.send_response(200)
-        #self.protocol_version = 'HTTP/1.1'
-        self.send_header('Content-Length', len(doc))
-        self.send_header('Content-Type', self.ctype)
-        self.end_headers()
-        self.wfile.write(doc)
-        self.wfile.write('\n')
-        if cf.debug > 0:
-            self.log_message('DEBUG: reply body=%s' % doc)
+        self._send_response(doc, self.ctype)
         return
 
     def do_GET(self):
-        self._log_initmsg()
+        if self._check_request() == False:
+            return
         if opt.sec_lv and self._check_acl_san() == False:
             self.send_error(401)
             return
         if self.path == '/wsdl':
             fiap = fiapProto.fiapProto(strict_check=True, debug=cf.debug)
             doc = fiap.getwsdl()
-            # XXX python can't handle send_header() this case.
-            #self.send_header('Content-Length', len(doc))
-            #self.send_header('Content-Type', 'text/xml; charset=utf-8');
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(doc)
-            self.wfile.write('\n')
+            self._send_response(doc, 'text/xml; charset=utf-8')
             return
         elif self.path.startswith('/?'):
             fiap = fiapProto.fiapProto(strict_check=True, debug=cf.debug)
@@ -112,12 +144,7 @@ class fiapHandler(BaseHTTPRequestHandler):
                 self.log_message('ERROR: %s' % fiap.getemsg())
                 self.send_error(403, fiap.getemsg())
                 return
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(doc)
-            self.wfile.write('\n')
-            if cf.debug > 0:
-                self.log_message('DEBUG: reply body=%s' % doc)
+            self._send_response(doc, 'text/json; charset=utf-8')
             return
         else:
             msg = 'unknown path, %s' % self.path
@@ -126,7 +153,8 @@ class fiapHandler(BaseHTTPRequestHandler):
             return
 
     def do_PUT(self):
-        self._log_initmsg()
+        if self._check_request() == False:
+            return
         msg = 'POST is not allowed.'
         self.log_message(msg)
         self.send_error(405, msg)
